c384cf2b97849d3015ab3852b03c9846dd926a07
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-05-03 12:33:14 -0400

    printing someone's name

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_systems(Startup, setup).run()
+    App::new()
+        .add_systems(Startup, setup)
+        .add_systems(Update, print_names)
+        .run()
 }
 
 pub fn setup(mut commands: Commands) {
     commands.spawn(Person {
         name: "Alex".to_string(),
     });
+}
+
+pub fn print_names(person_query: Query<&Person>) {
+    for person in person_query.iter() {
+        println!("Name : {}", person.name);
+    }
 }
 
 #[derive(Component)]
