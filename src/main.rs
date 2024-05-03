53ca9d2e290bef4f569dd0eb303fbb04e959d971
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-05-03 12:57:51 -0400

    add a job to the people

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,29 @@ fn main() {
 }
 
 pub fn setup(mut commands: Commands) {
-    commands.spawn(Person {
-        name: "Alex".to_string(),
-    });
-    commands.spawn(Person {
-        name: "Bob".to_string(),
-    });
-    commands.spawn(Person {
-        name: "Sylvain".to_string(),
-    });
-    commands.spawn(Person {
-        name: "Mathiew".to_string(),
-    });
+    commands.spawn((
+        Person {
+            name: "Alex".to_string(),
+        },
+        Employed { job: Job::Doctor },
+    ));
+    commands.spawn((
+        Person {
+            name: "Bob".to_string(),
+        },
+        Employed { job: Job::Lawyer },
+    ));
+    commands.spawn((
+        Person {
+            name: "Sylvain".to_string(),
+        },
+        Employed {
+            job: Job::FireFighter,
+        },
+    ));
+    commands.spawn((Person {
+        name: "Mathew".to_string(),
+    },));
 }
 
 pub fn print_names(person_query: Query<&Person>) {
@@ -31,4 +42,16 @@ pub fn print_names(person_query: Query<&Person>) {
 #[derive(Component)]
 pub struct Person {
     pub name: String,
+}
+
+#[derive(Component)]
+pub struct Employed {
+    pub job: Job,
+}
+
+#[derive(Debug)]
+pub enum Job {
+    Doctor,
+    FireFighter,
+    Lawyer,
 }
