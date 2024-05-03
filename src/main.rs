fd2643aecbb8b0553dd64b1e0e8d640441a386bd
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-05-03 18:03:09 -0400

    git status

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ fn main() {
     App::new()
         .add_systems(Startup, setup)
         .add_systems(Update, print_names)
+        .add_systems(Update, people_with_jobs)
+        .add_systems(Update, people_ready_for_hire)
+        .add_systems(Update, person_does_job)
         .run()
 }
 
@@ -36,6 +39,29 @@ pub fn setup(mut commands: Commands) {
 pub fn print_names(person_query: Query<&Person>) {
     for person in person_query.iter() {
         println!("Name : {}", person.name);
+    }
+}
+
+pub fn people_with_jobs(person_query: Query<&Person, With<Employed>>) {
+    for person in person_query.iter() {
+        println!("{} has a job", person.name);
+    }
+}
+
+pub fn people_ready_for_hire(person_query: Query<&Person, Without<Employed>>) {
+    for person in person_query.iter() {
+        println!("{} is ready for hire", person.name);
+    }
+}
+
+pub fn person_does_job(person_query: Query<(&Person, &Employed)>) {
+    for (person, employed) in person_query.iter() {
+        let job_name = match employed.job {
+            Job::Doctor => "Doctor",
+            Job::FireFighter => "Fire Fighter",
+            Job::Lawyer => "Lawyer",
+        };
+        println!("{0} is a {1}", person.name, job_name);
     }
 }
 
