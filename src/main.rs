265ed45400690e069e99b91d3c63164f11cdf98c
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-05-04 22:23:28 -0400

    make the systems so that we always stay inside the window

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 
+pub const PLAYER_SIZE: f32 = 54.0; // This is the player sprite size
+pub const PLAYER_SPEED: f32 = 500.0;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, spawn_player)
         .add_systems(Startup, spawn_camera)
         .add_systems(Update, player_movement)
+        .add_systems(Update, confine_player_movement)
         .run()
 }
 
@@ -39,8 +43,6 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
     });
 }
 
-pub const PLAYER_SPEED: f32 = 500.0;
-
 pub fn player_movement(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut player_query: Query<&mut Transform, With<Player>>,
@@ -67,5 +69,37 @@ pub fn player_movement(
         }
 
         transform.translation += direction * PLAYER_SPEED * time.delta_seconds();
+    }
+}
+
+pub fn confine_player_movement(
+    mut player_query: Query<&mut Transform, With<Player>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    if let Ok(mut player_transform) = player_query.get_single_mut() {
+        let window = window_query.get_single().unwrap();
+
+        let half_player_size = PLAYER_SIZE / 2.0;
+        let x_min = 0.0 + half_player_size;
+        let x_max = window.width() - half_player_size;
+        let y_min = 0.0 + half_player_size;
+        let y_max = window.height() - half_player_size;
+
+        let mut translation = player_transform.translation;
+
+        // Bound the player to x position
+        if translation.x < x_min {
+            translation.x = x_min;
+        } else if translation.x > x_max {
+            translation.x = x_max;
+        }
+        // Bound the players y position.
+        if translation.y < y_min {
+            translation.y = y_min;
+        } else if translation.y > y_max {
+            translation.y = y_max;
+        }
+
+        player_transform.translation = translation;
     }
 }
