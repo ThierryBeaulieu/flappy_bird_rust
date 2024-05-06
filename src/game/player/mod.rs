e1929887e19aaa68bea41a78ff8e975f73e91483
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-05-05 23:45:19 -0400

    update player system

--- src/game/player/mod.rs
+++ src/game/player/mod.rs
@@ -5,6 +5,10 @@ mod systems;
 
 use systems::*;
 
+use crate::AppState;
+
+use super::SimulationState;
+
 #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
 pub struct MovementSystemSet;
 
@@ -16,14 +20,31 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.configure_sets(Update, MovementSystemSet.before(ConfinementSystemSet))
+        app
+            // Configure System Sets
+            .configure_sets(Update, MovementSystemSet.before(ConfinementSystemSet))
             // Startup Systems
             .add_systems(Startup, spawn_player)
             // Systems
-            .add_systems(Update, player_movement.in_set(MovementSystemSet))
-            .add_systems(Update, confine_player_movement.in_set(ConfinementSystemSet))
-            .add_systems(Update, player_movement)
-            .add_systems(Update, enemy_hit_player)
-            .add_systems(Update, player_hit_star);
+            .add_systems(
+                Update,
+                player_movement
+                    .in_set(MovementSystemSet)
+                    .run_if(in_state(AppState::Game))
+                    .run_if(in_state(SimulationState::Running)),
+            )
+            .add_systems(
+                Update,
+                confine_player_movement
+                    .in_set(ConfinementSystemSet)
+                    .run_if(in_state(AppState::Game))
+                    .run_if(in_state(SimulationState::Running)),
+            )
+            .add_systems(
+                Update,
+                (enemy_hit_player, player_hit_star)
+                    .run_if(in_state(AppState::Game))
+                    .run_if(in_state(SimulationState::Running)),
+            );
     }
 }
