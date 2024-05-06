874ce985c4160a929e0b1da2e593d6400be6af78
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-05-05 20:24:42 -0400

    in set

--- src/game/enemy/mod.rs
+++ src/game/enemy/mod.rs
@@ -7,6 +7,9 @@ mod systems;
 use resources::*;
 use systems::*;
 
+use crate::game::SimulationState;
+use crate::AppState;
+
 pub const NUMBER_OF_ENEMIES: usize = 5;
 pub const ENEMY_SPEED: f32 = 200.0;
 pub const ENEMY_SIZE: f32 = 64.0; // This is the enemy sprite size
@@ -15,12 +18,23 @@ pub struct EnemyPlugin;
 
 impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<EnemySpawnTimer>()
+        app
+            // Init
+            .init_resource::<EnemySpawnTimer>()
+            // Startup
             .add_systems(Startup, spawn_enemies)
-            .add_systems(Update, enemy_movement)
-            .add_systems(Update, confine_enemy_movement)
-            .add_systems(Update, update_enemy_direction)
-            .add_systems(Update, tick_enemy_spawn_timer)
-            .add_systems(Update, spawn_enemies_over_time);
+            // Updatedate, enemy_movement)
+            .add_systems(
+                Update,
+                (
+                    enemy_movement,
+                    confine_enemy_movement,
+                    update_enemy_direction,
+                    tick_enemy_spawn_timer,
+                    spawn_enemies_over_time,
+                )
+                    .in_set(in_state(AppState::Game))
+                    .in_set(in_state(SimulationState::Running)),
+            );
     }
 }
