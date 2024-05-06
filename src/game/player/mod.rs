b6c3ff0be99110667d9e477234f40458ba6781ff
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-05-05 23:38:55 -0400

    add system set

--- src/game/player/mod.rs
+++ src/game/player/mod.rs
@@ -4,12 +4,24 @@ pub mod components;
 mod systems;
 
 use systems::*;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct MovementSystemSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+
+pub struct ConfinementSystemSet;
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_player)
-            .add_systems(Update, confine_player_movement)
+        app.configure_sets(Update, MovementSystemSet.before(ConfinementSystemSet))
+            // Startup Systems
+            .add_systems(Startup, spawn_player)
+            // Systems
+            .add_systems(Update, player_movement.in_set(MovementSystemSet))
+            .add_systems(Update, confine_player_movement.in_set(ConfinementSystemSet))
             .add_systems(Update, player_movement)
             .add_systems(Update, enemy_hit_player)
             .add_systems(Update, player_hit_star);
