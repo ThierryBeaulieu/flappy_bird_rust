e8903cf2b7ac66ec46e29a4e791a14da02a18cd2
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-05-06 21:17:04 -0400

    refactor

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -19,12 +19,18 @@ pub struct GamePlugin;
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.init_state::<SimulationState>()
+            // Events
             .add_event::<GameOver>()
+            // On Enter Systems
+            .add_systems(OnEnter(AppState::Game), pause_simulation)
+            // Plugin
             .add_plugins(EnemyPlugin)
             .add_plugins(PlayerPlugin)
             .add_plugins(ScorePlugin)
             .add_plugins(StarPlugin)
-            .add_systems(Update, toggle_simulation.run_if(in_state(AppState::Game)));
+            .add_systems(Update, toggle_simulation.run_if(in_state(AppState::Game)))
+            // On Exit System
+            .add_systems(OnExit(AppState::Game), resume_simulation);
     }
 }
 
