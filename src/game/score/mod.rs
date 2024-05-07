2f8290bd7c32777e5a8e7dd6e068d0d441a9295e
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-05-06 20:23:06 -0400

    finish adding state in mod.rs

--- src/game/score/mod.rs
+++ src/game/score/mod.rs
@@ -6,14 +6,23 @@ mod systems;
 use resources::*;
 use systems::*;
 
+use crate::AppState;
+
 pub struct ScorePlugin;
 
 impl Plugin for ScorePlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<Score>()
+        app
+            // Resources
+            .init_resource::<Score>()
             .init_resource::<HighScores>()
-            .add_systems(Update, update_score)
+            // On Enter State
+            .add_systems(OnEnter(AppState::Game), insert_score)
+            // Systems
+            .add_systems(Update, update_score.run_if(in_state(AppState::Game)))
             .add_systems(Update, update_high_scores)
-            .add_systems(Update, high_scores_updated);
+            .add_systems(Update, high_scores_updated)
+            // On Exit State
+            .add_systems(OnExit(AppState::Game), remove_score);
     }
 }
--- src/systems.rs
+++ src/systems.rs
@@ -2,7 +2,7 @@ use bevy::app::AppExit;
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 
-use crate::{events::*, AppState};
+use crate::{events::*, game::SimulationState, AppState};
 
 pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
     let window = window_query.get_single().unwrap();
@@ -57,6 +57,7 @@ pub fn transition_to_main_menu_state(
             AppState::Game => {}
             _ => {
                 commands.insert_resource(NextState(Some(AppState::MainMenu)));
+                commands.insert_resource(NextState(Some(SimulationState::Paused)));
                 println!("Entered AppState::MainMenu ");
             }
         }
