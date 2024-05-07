40ae1ea1261941fae5ca7e75b6b00ea812d805ac
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-05-06 20:28:11 -0400

    able to return to the menu before

--- src/systems.rs
+++ src/systems.rs
@@ -22,9 +22,10 @@ pub fn exit_game(
     }
 }
 
-pub fn handle_game_over(mut game_over_event_reader: EventReader<GameOver>) {
+pub fn handle_game_over(mut commands: Commands, mut game_over_event_reader: EventReader<GameOver>) {
     for event in game_over_event_reader.read() {
         println!("Your final score is : {}", event.score.to_string());
+        commands.insert_resource(NextState(Some(AppState::GameOver)))
     }
 }
 
