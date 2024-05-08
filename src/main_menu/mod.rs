df6e4d7ff554e57d90f272a2b5bb1fa4ecfe9ea6
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-05-07 20:21:32 -0400

    add main_menu

--- src/main_menu/mod.rs
+++ src/main_menu/mod.rs
@@ -4,14 +4,18 @@ mod systems;
 
 use bevy::prelude::*;
 
+use crate::AppState;
+
+use self::systems::layout::{despawn_main_menu, spawn_main_menu};
+
 pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, main_menu);
+        app
+            // OnEnter State Systems
+            .add_systems(OnEnter(AppState::MainMenu), spawn_main_menu)
+            // OnExit State Systems
+            .add_systems(OnExit(AppState::MainMenu), despawn_main_menu);
     }
-}
-
-pub fn main_menu() {
-    println!("You are the main menu.");
 }
